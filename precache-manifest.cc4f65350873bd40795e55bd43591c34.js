b491ff2949925e3e0f3fa7f5557ca97cd18cb81d
harsh <harsh710pat@gmail.com>
2020-09-13 01:13:50 +0530

    Updates

---
+++ precache-manifest.cc4f65350873bd40795e55bd43591c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0450e85013f010d2e8ac9680c75380d",
+    "url": "/Harsh71019/CovidtrackerLive/index.html"
+  },
+  {
+    "revision": "b2055a71508c9538bfc3",
+    "url": "/Harsh71019/CovidtrackerLive/static/css/main.d21ee2c3.chunk.css"
+  },
+  {
+    "revision": "e37f1882c8bcad9d9cdc",
+    "url": "/Harsh71019/CovidtrackerLive/static/js/2.b980bbd0.chunk.js"
+  },
+  {
+    "revision": "9e10694be4ec7085ebb25e3804b0b3ab",
+    "url": "/Harsh71019/CovidtrackerLive/static/js/2.b980bbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2055a71508c9538bfc3",
+    "url": "/Harsh71019/CovidtrackerLive/static/js/main.e0079a21.chunk.js"
+  },
+  {
+    "revision": "4cb61e773585369f44d0",
+    "url": "/Harsh71019/CovidtrackerLive/static/js/runtime-main.c7eef70c.js"
+  },
+  {
+    "revision": "79bcaa6d98ac2e9de62d0f8d4449283e",
+    "url": "/Harsh71019/CovidtrackerLive/static/media/1.79bcaa6d.jpg"
+  }
+]);
